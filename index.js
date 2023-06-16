1849715ebde4071abdf043210128aa83b248df70
Katia <kushnir.ekaterina@gmail.com>
2023-06-16 14:28:33 +0200

    Execute and listen requests

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 
+// Define the first route of APP
+app.get('/', (req, res) => {
+    res.send('APP Express + TS + Swagger + Mongoose');
+});
+
+// Execute APP and listen requests to PORT
+app.listen(port, () => {
+    console.log(`EXPRESS SERVER: Running at http://localhost:${port}`);
+});
