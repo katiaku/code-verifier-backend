c50eba07d163f561c0682c3316f765ef0cca6322
Katia <kushnir.ekaterina@gmail.com>
2023-06-16 18:02:14 +0200

    Configuration TS

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const port = process.env.PORT || 8000;
+app.get('/', (req, res) => {
+    res.send('Welcome to API Restful: Express + TS + Nodemon + Jest + Swagger + Mongoose');
+});
+app.listen(port, () => {
+    console.log(`EXPRESS SERVER: Running at http://localhost:${port}`);
+});
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,15 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port: string | number = process.env.PORT || 8000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Welcome to API Restful: Express + TS + Nodemon + Jest + Swagger + Mongoose');
+});
+
+app.listen(port, () => {
+    console.log(`EXPRESS SERVER: Running at http://localhost:${port}`);
+});
