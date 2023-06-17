22caafde06ceb7589e12c527e1858fb40f1e2adf
Katia <kushnir.ekaterina@gmail.com>
2023-06-17 19:51:33 +0200

    Create HelloRouter

---
+++ src/routes/HelloRouter.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from "express";
+import { HelloController } from "@/controller/HelloController";
+import { LogInfo } from "@/utils/logger";
+
+// Router from express
+let helloRouter = express.Router();
+
+// http://localhost:8000/api/hello/
+helloRouter.route('/')
+    // GET:
+    .get(async(req: Request, res: Response) => {
+        // Obtain a Query Param
+        let name: any = req?.query?.name;
+        LogInfo(`Query Param: ${name}`);
+        // Controller Instance to execute method
+        const controller: HelloController = new HelloController();
+        // Obtain Response
+        const response = await controller.getMessage(name);
+        // Send the response to the client
+        return res.send(response);
+    })
