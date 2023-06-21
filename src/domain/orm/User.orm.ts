2a23d12921ec868919d0734fcdca083baa21729f
Katia <kushnir.ekaterina@gmail.com>
2023-06-21 18:55:52 +0200

    Create userEntity

---
+++ src/domain/orm/User.orm.ts
@@ -0,0 +1,24 @@
+import { userEntity } from "../entities/User.entity";
+import { LogSuccess, LogError } from "../../utils/logger";
+
+// CRUD
+
+/**
+ * Method to obtain all users from collection "Users" in Mongo Server
+ */
+export const GetAllUsers = async (): Promise<any[] | undefined> => {
+    try {
+        let userModel = userEntity();
+        // Search all users
+        return await userModel.find({ isDeleted: false })
+    } catch (error) {
+        LogError(`[ORM ERROR]: Getting All Users: ${error}`);
+    }
+}
+
+// TODO:
+// - Get User By ID
+// - Get User By Email
+// - Delete User By ID
+// - Create New User
+// - Update User By ID
